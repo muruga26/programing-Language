d8eaf6b131083a4b823f1cdad0d6a23ac7c3d194
Sivasomasekaran <34641391+Sivasomasekaran@users.noreply.github.com>
2017-12-19 10:09:58 +0530

    Create variables.c

---
+++ C-Programming/Beginner Level/variables.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{ 
+   int a;
+   char b[100];
+   float c;
+   scanf("%d",&a);
+   scanf("%s",&b);
+   scanf("%f",&c);
+   printf("%d",a);
+   printf("%s",b);
+   printf("%f",c);
+}
