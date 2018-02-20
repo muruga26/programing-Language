81f7a3bb7fec933c0036e420836958c0c6ce8d3b
Sivasomasekaran <34641391+Sivasomasekaran@users.noreply.github.com>
2018-02-20 21:18:22 +0530

    Create maximum element.c

---
+++ C-Programming/Beginner Level/3set/maximum element.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+int main(void) {
+	int i,j,n,a[i];
+	
+	scanf("%d ",&n);
+	for( i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			if(a[i]>a[j])
+			{
+				int tmp=a[i];
+				a[i]=a[j];
+				a[j]=tmp;
+			}
+		}
+	}
+	printf("%d",a[0]);
+	return 0;
+}
