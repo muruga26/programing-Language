333cad93503202a3af7d1f711aa0ef0ccb9690b2
Sivasomasekaran <34641391+Sivasomasekaran@users.noreply.github.com>
2018-02-20 20:27:23 +0530

    Create amstrong.c

---
+++ C-Programming/Beginner Level/2set/amstrong.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+int main(void) {
+	int a,b,r,sum=0,count=0;
+	
+	scanf(" %d",&a);
+	b=a;
+	while(a>0)
+	{
+		a=a/10;
+		count++;
+	}
+	a=b;
+	while(a>0)
+	{
+		r=a%10;
+		sum=sum+pow(r,count);
+		a=a/10;
+	}
+	if(sum==b)
+	printf("yes");
+	else
+	printf("no");
+	return 0;
+}
