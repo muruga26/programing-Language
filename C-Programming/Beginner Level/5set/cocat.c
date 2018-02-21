2a1d4708b8b42ee3f42a4a051001009859a35d15
Sivasomasekaran <34641391+Sivasomasekaran@users.noreply.github.com>
2018-02-21 20:53:27 +0530

    Create cocat.c

---
+++ C-Programming/Beginner Level/5set/cocat.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void)
+{
+
+  char str1[50], str2[50], i, j;
+   scanf("%s",str1);
+   scanf("%s",str2);
+   for(i=0; str1[i]!='\0'; ++i); 
+
+   for(j=0; str2[j]!='\0'; ++j, ++i)
+   {
+      str1[i]=str2[j];
+   }
+  
+   str1[i]='\0';
+   printf("%s",str1);
+  return 0;
+}
