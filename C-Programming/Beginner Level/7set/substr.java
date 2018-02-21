b9a29e518a00cc9d750c6a92b33a066e230ac047
Sivasomasekaran <34641391+Sivasomasekaran@users.noreply.github.com>
2018-02-21 15:34:08 +0530

    Create substr.java

---
+++ C-Programming/Beginner Level/7set/substr.java
@@ -0,0 +1,23 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String a;
+		int n;
+		a=sc.next();
+		n=sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			System.out.print(a.charAt(i));
+		}
+		
+	}
+}
