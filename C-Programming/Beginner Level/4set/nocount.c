7855f76922e4661b4d4a272c692d31934520743e
Sivasomasekaran <34641391+Sivasomasekaran@users.noreply.github.com>
2018-02-21 19:54:38 +0530

    Create nocount.c

---
+++ C-Programming/Beginner Level/4set/nocount.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+
+	char ch[20];
+	int i,a=0;
+	gets(ch);
+	for(i=0;ch[i]!=NULL;i++)
+	{
+		if(ch[i]>='0' && ch[i]<='9')
+		{
+		a++;	
+		}
+	}
+	printf("%d",a);
+	return 0;
+}
