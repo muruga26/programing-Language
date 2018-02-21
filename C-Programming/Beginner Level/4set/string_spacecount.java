fc7ab0b7dd7b2e99530d2aafffc1b876f85bd8fd
Sivasomasekaran <34641391+Sivasomasekaran@users.noreply.github.com>
2018-02-21 19:48:36 +0530

    Create string_spacecount.java

---
+++ C-Programming/Beginner Level/4set/string_spacecount.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String str;
+		int len;
+		Scanner s=new Scanner(System.in);
+		str=s.nextLine();
+		len=str.length();
+		String a[]=str.split(" ");
+		System.out.println(a.length-1);
+		
+	}
+}
