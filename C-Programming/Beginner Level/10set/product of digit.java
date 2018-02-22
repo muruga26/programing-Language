7c03494242c58085aa11cc4587c787269189b614
Sivasomasekaran <34641391+Sivasomasekaran@users.noreply.github.com>
2018-02-22 15:39:19 +0530

    Create product of digit.java

---
+++ C-Programming/Beginner Level/10set/product of digit.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class product{
+    public static void main(String aa[]){
+    String a;
+    int d;
+    Scanner s=new Scanner(System.in);
+    d=s.nextInt();
+    a=String.valueOf(d);
+    char v[]=a.toCharArray();
+    int sum=1;
+    for(int i=0;i<v.length;i++){
+        int y=Character.getNumericValue(v[i]);
+        sum=sum*y;
+    }
+    System.out.println(sum);
+   
+   
+           
+   }
+}
